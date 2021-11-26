9fc14b7607847b89ffe68942f089f58e0677b3ce
Saull Brandão <saull@outlook.com>
2021-11-26 11:50:17 -0300

    feat: home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from '@chakra-ui/react'
+import { SimpleGrid, Divider, Text, Box, Flex } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { Banner } from '../components/Banner'
 import { Header } from '../components/Header'
@@ -6,10 +6,10 @@ import { TravelType } from '../components/TravelType'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <Flex maxW="100vw" direction="column">
       <Header />
       <Banner />
-      <SimpleGrid as="ul" columns={5} mt="114" mx="140">
+      <SimpleGrid as="ul" columns={5} my="120" mx="140">
         <TravelType
           image="/images/cocktail.svg"
           imageAlt="cocktail"
@@ -32,7 +32,37 @@ const Home: NextPage = () => {
           title="e mais..."
         />
       </SimpleGrid>
-    </>
+      <Divider
+        width="90px"
+        border="2px solid"
+        borderColor="gray.600"
+        opacity="1"
+        mx="auto"
+      />
+      <Text
+        textAlign="center"
+        my="52px"
+        fontSize="36px"
+        lineHeight="54px"
+        fontWeight={500}
+      >
+        Vamos nessa? <br />
+        Então escolha seu continente
+      </Text>
+
+      <Box
+        mx="auto"
+        mb="10"
+        w="100%"
+        maxW="1240px"
+        h="450px"
+        bg="gray.200"
+        textAlign="center"
+        align="center"
+      >
+        Carousel
+      </Box>
+    </Flex>
   )
 }
 
