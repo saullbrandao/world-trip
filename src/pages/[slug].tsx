52449a1961e6f2c2ad930c15e8a48e18b239931b
Saull Brandão <saull@outlook.com>
2021-11-30 16:37:40 -0300

    feat: make the continent page responsive

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -39,30 +39,41 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
   return (
     <Flex maxW="100vw" direction="column">
       <Header showBackButton />
-      <Box
+      <Flex
         bg={`linear-gradient(0deg, rgba(28, 20, 1, 0.35), rgba(28, 20, 1, 0.35)), url(${continent.imageUrl})`}
         bgPosition="center"
         bgRepeat="no-repeat"
+        justify={['center', 'center', 'normal']}
+        align={['center', 'center', 'normal']}
         bgSize="cover"
         w="100%"
-        h="500"
+        h={['150px', '500px']}
         position="relative"
-        mb="80px"
+        mb={['24px', '80px']}
       >
         <Text
           fontWeight="600"
-          fontSize="48px"
-          lineHeight="72px"
+          fontSize={['28px', '38px', '48px']}
+          lineHeight={['48px', '60px', '72px']}
           color="gray.100"
-          position="absolute"
-          left="140px"
-          bottom="60px"
+          position={['relative', 'relative', 'absolute']}
+          left={['0', '0', '140px']}
+          bottom={['0', '0', '60px']}
         >
           {continent.title}
         </Text>
-      </Box>
-      <SimpleGrid columns={2} spacing="70px" mx="140px" mb="80px">
-        <Text textAlign="justify" fontSize="24px" lineHeight="36px">
+      </Flex>
+      <SimpleGrid
+        columns={[1, 1, 2]}
+        spacing={['16px', '43px', '70px']}
+        mx={['16px', '16px', '16px', '140px']}
+        mb={['40px', '60px', '80px']}
+      >
+        <Text
+          textAlign="justify"
+          fontSize={['14px', '19px', '19px', '24px']}
+          lineHeight={['21px', '21px', '26px', '36px']}
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. A eligendi
           ratione, voluptatibus veniam eveniet cumque perferendis aliquam
           mollitia repellat similique dolores rem exercitationem? Dignissimos
@@ -70,12 +81,12 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
           necessitatibus cupiditate odit officiis nihil! Lorem ipsum dolor sit
           amet consectetur.
         </Text>
-        <Flex justify="space-around" align="center">
-          <Stack justify="center" align="center" textAlign="center">
+        <Flex justify={['space-between', 'space-around']} align="center">
+          <Stack justify="center" align="center" textAlign="left">
             <Text
               as="span"
-              fontSize="48px"
-              lineHeight="72px"
+              fontSize={['24px', '36px', '36px', '48px']}
+              lineHeight={['36px', '54px', '54px', '72px']}
               fontWeight="600"
               color="orange.300"
             >
@@ -84,8 +95,8 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
             <Text
               as="span"
               color="gray.600"
-              fontSize="24px"
-              lineHeight="36px"
+              fontSize={['18px', '21px', '24px']}
+              lineHeight={['27px', '31px', '36px']}
               fontWeight="600"
             >
               Países
@@ -94,8 +105,8 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
           <Stack justify="center" align="center" textAlign="center">
             <Text
               as="span"
-              fontSize="48px"
-              lineHeight="72px"
+              fontSize={['24px', '36px', '36px', '48px']}
+              lineHeight={['36px', '54px', '54px', '72px']}
               fontWeight="600"
               color="orange.300"
             >
@@ -104,8 +115,8 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
             <Text
               as="span"
               color="gray.600"
-              fontSize="24px"
-              lineHeight="36px"
+              fontSize={['18px', '21px', '24px']}
+              lineHeight={['27px', '31px', '36px']}
               fontWeight="600"
             >
               Línguas
@@ -114,8 +125,8 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
           <Stack justify="center" align="center" textAlign="center">
             <Text
               as="span"
-              fontSize="48px"
-              lineHeight="72px"
+              fontSize={['24px', '36px', '36px', '48px']}
+              lineHeight={['36px', '54px', '54px', '72px']}
               fontWeight="600"
               color="orange.300"
             >
@@ -124,8 +135,8 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
             <Text
               as="span"
               color="gray.600"
-              fontSize="24px"
-              lineHeight="36px"
+              fontSize={['18px', '21px', '24px']}
+              lineHeight={['27px', '31px', '36px']}
               fontWeight="600"
             >
               Cidades +100
@@ -134,10 +145,16 @@ const Continent: NextPage<ContinentProps, JSX.Element> = ({ continent }) => {
         </Flex>
       </SimpleGrid>
 
-      <Heading mx="140px" mb="40px">
+      <Heading
+        fontSize={['24px', '30px', '36px']}
+        lineHeight={['36px', '45px', '54px']}
+        fontWeight="500"
+        mx={['16px', '140px']}
+        mb={['20px', '30px', '40px']}
+      >
         Cidades +100
       </Heading>
-      <Wrap mb="35px" mx="140px" flexWrap="wrap">
+      <Wrap mb="35px" mx={['60px', '140px']} flexWrap="wrap">
         {continent.topDestinations.map(city => (
           <WrapItem key={city.name}>
             <CityCard city={city} />
